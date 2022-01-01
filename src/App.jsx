28a14df39677cf4d1eb92c1f0fcc0208aeecc96c
Jorge <jorgelartearteagap@gmail.com>
2022-01-01 12:07:42 -0400

    [FEAT] Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import {
+  Footer,
+  Navbar,
+  Services,
+  Transactions,
+  Loader,
+  Welcome
+} from './components'
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Loader />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
