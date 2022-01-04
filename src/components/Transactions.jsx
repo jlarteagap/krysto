cc40d4ec42f4eca15614f22cfb157a5a6ab10e04
Jorge <jorgelartearteagap@gmail.com>
2022-01-04 12:14:49 -0400

    [FEAT] Get All transactions

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -2,7 +2,6 @@
 import React, { useContext } from 'react'
 import { TransactionContext } from '../context/TransactionContext'
 import { shortenAddress } from '../utils/shortenAddress'
-import dummyData from '../utils/dummyData'
 import useFetch from '../hooks/useFetch'
 
 const TransactionCard = ({
@@ -67,7 +66,7 @@ const TransactionCard = ({
   )
 }
 const Transactions = () => {
-  const { currentAccount } = useContext(TransactionContext)
+  const { currentAccount, transactions } = useContext(TransactionContext)
   return (
     <div className="flex w-full justify-center items-center 2xl px-20 gradient-bg-transactions">
       <div className="flex flex-col md:p-12 py-12 px-4">
@@ -81,7 +80,7 @@ const Transactions = () => {
           </h3>
         )}
         <div className="flex flex-wrap justify-center items-center mt-10">
-          {dummyData.reverse().map((transaction, i) => (
+          {transactions.reverse().map((transaction, i) => (
             <TransactionCard key={i} {...transaction} />
           ))}
         </div>
--- src/context/TransactionContext.jsx
+++ src/context/TransactionContext.jsx
@@ -32,23 +32,59 @@ export const TransactionProvider = ({ children }) => {
   const [transactionCount, setTransactionCount] = useState(
     localStorage.getItem('transactionCount')
   )
+  const [transactions, setTransactions] = useState([])
 
   const handleChange = (e, name) => {
     setFormData(prevState => ({ ...prevState, [name]: e.target.value }))
   }
+  const getAllTransactions = async () => {
+    try {
+      if (!ethereum) return alert('Please install metamask')
+      const transactionContract = getEthereumContract()
+      const availableTransactions =
+        await transactionContract.getAllTransactions()
 
+      const structuredTransactions = availableTransactions.map(transaction => ({
+        addressTo: transaction.receiver,
+        addressFrom: transaction.sender,
+        timestamp: new Date(
+          transaction.timestamp.toNumber() * 1000
+        ).toLocaleDateString(),
+        message: transaction.message,
+        keyword: transaction.keyword,
+        amount: parseInt(transaction.amount._hex) / 10 ** 18
+      }))
+
+      setTransactions(structuredTransactions)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   const checkIfWalletIsConnect = async () => {
     try {
       if (!ethereum) return alert('Please install metamask')
       const accounts = await ethereum.request({ method: 'eth_accounts' })
       if (accounts.length) {
         setCurrentAccount(accounts[0])
+        getAllTransactions()
       } else {
         console.log('No accounts found')
       }
     } catch (error) {
       console.error(error)
 
+      throw new Error('No ethereum object')
+    }
+  }
+
+  const checkIfTransactionExist = async () => {
+    try {
+      const transactionContract = getEthereumContract()
+      const transactionCount = await transactionContract.getTransactionCount()
+
+      window.localStorage.setItem('transactionCount', transactionCount)
+    } catch (error) {
+      console.error(error)
       throw new Error('No ethereum object')
     }
   }
@@ -112,6 +148,7 @@ export const TransactionProvider = ({ children }) => {
 
   useEffect(() => {
     checkIfWalletIsConnect()
+    checkIfTransactionExist()
   }, [])
 
   return (
@@ -122,7 +159,9 @@ export const TransactionProvider = ({ children }) => {
         formData,
         setFormData,
         handleChange,
-        sendTransaction
+        sendTransaction,
+        transactions,
+        isLoading
       }}
     >
       {children}
